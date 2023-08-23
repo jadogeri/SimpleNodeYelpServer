f3cd3ab89ce7e629e871f1abc435395177a7a9c1
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2023-08-23 12:27:06 -0500

    Update server.js with api key for testing

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const ax = axios.create({
     baseURL: `https://api.yelp.com/v3/businesses`,
     headers: {
 
-        Authorization: "Bearer *************************************************"
+        Authorization: "Bearer yT8rvGixLKUs09F3M5JjD6hL2Va5CItaCJ2tOOKRtPKlLQbVh_SQS55BumNjaRkY3DFE4K0GsnI5OqYACwpCaG2VfHS32Kr7JGxLIRAwpEwvJio1P1QyaVXJoaTdZHYx"
 
     },
 
